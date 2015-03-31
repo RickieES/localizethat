33d813ebf8c6a7900f15d497a20ba5cd2c94a2ba
rpalomares <rpmdisguise-java@yahoo.es>
2015-03-31 17:08:18 +0200

    New renderer for JLists that allows to display a list of JCheckBoxes with items to provide a multple selection list

---
+++ src/net/localizethat/gui/renderers/SelectableListItem.java
@@ -0,0 +1,43 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package net.localizethat.gui.renderers;
+
+import java.awt.Component;
+import javax.swing.JCheckBox;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+import javax.swing.UIManager;
+import net.localizethat.gui.models.SelectableItem;
+
+/**
+ *
+ * @author rpalomares
+ */
+public class SelectableListItem extends JCheckBox
+        implements ListCellRenderer {
+
+    public SelectableListItem() {
+      setForeground(UIManager.getColor("List.foreground"));
+      setBackground(UIManager.getColor("List.background"));
+    }
+
+    @Override
+    public Component getListCellRendererComponent(JList list, Object value,
+            int index, boolean isSelected, boolean cellHasFocus) {
+        SelectableItem item = (SelectableItem) value;
+        setEnabled(list.isEnabled());
+        setSelected(item.isSelected());
+        setFont(list.getFont());
+        setText(value.toString());
+        if (item.isSelected()) {
+            this.setBackground(UIManager.getColor("List.selectionBackground"));
+        } else {
+            this.setBackground(UIManager.getColor("List.background"));
+        }
+      return this;
+    }
+
+}
