2be8983fd5534a2e09942b72d8af65a018a7ebf8
rpalomares <rpmdisguise-java@yahoo.es>
2015-08-27 11:24:17 +0200

    Issue #39 - Bring app version to 0.8

--- src/net/localizethat/Main.java
+++ src/net/localizethat/Main.java
@@ -32,7 +32,7 @@ public class Main {
      * - Minor: 0 == 0
      * - Bugrelease: "b4" &lt; "r3"
      */
-    public static final String version = "0.7";
+    public static final String version = "0.8";
 
     /**
      * Reference to the application settings
